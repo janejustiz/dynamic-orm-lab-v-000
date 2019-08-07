3a35e1c004df5b4bafe534916de8d5d68d8d4626
Jane Justiz <janejustiz@gmail.com>
2019-08-07 07:44:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ class InteractiveRecord
   end
 
   def self.find_by(thing)
-    sql = "SELECT * FROM #{self.table_name} WHERE thing.keys = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE thing.keys.first = ?"
     DB[:conn].execute(sql, "thing[thing.keys[0]]")
   end
 
